ef6fa0c305028133e47b1252ff6bcd0d6148a8a8
Marcos Couto <marcoscouto1997@gmail.com>
2023-04-15 18:53:29 -0300

    implementação dos requisitos post messages

--- src/app.js
+++ src/app.js
@@ -71,25 +71,48 @@ app.post("/participants", async (req, res) => {
     }
 })
 
-app.get("/participants", (req, res) => {
-    db.collection("participants").find().toArray()
-        .then(participants => res.send(participants))
-        .catch((err) => console.log(err.message))
+app.get("/participants", async (req, res) => {
+
+    const resp = await db.collection("participants").find().toArray()
+
+    try {
+        res.send(resp)
+    } catch (err) {
+        console.log(err.message)
+    }
 })
 
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
     const { user } = req.headers;
 
-    if (!to || !text || !type) {
-        return res.send(422)
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().required()
+    })
+
+    const validation = messageSchema.validate(req.body, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422)
     }
 
-    db.collection("messages").insertOne({ from: name, to, text, type, time: "HH:mm:ss" })
-        .then(() => res.sendStatus(201))
-        .catch((err) => console.log(err.message))
+    try {
+        await db.collection("messages").insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time: hour
+        })
 
-
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err.message)
+        return res.sendStatus(422)
+    }
 })
 
 app.get("/messages", (res, req) => {
