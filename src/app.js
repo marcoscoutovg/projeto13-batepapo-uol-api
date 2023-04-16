c23a9b39c894e618700c5e84a60fa47856360402
Marcos Couto <marcoscouto1997@gmail.com>
2023-04-16 10:55:32 -0300

    ajustes para testar no avaliador

--- src/app.js
+++ src/app.js
@@ -99,7 +99,7 @@ app.post("/messages", async (req, res) => {
 
     if (validation.error) {
         const errors = validation.error.details.map(detail => detail.message)
-        return res.status(422)
+        return res.status(422).send(errors)
     }
 
     try {
